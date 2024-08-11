e4a07b6390e2b53638a52ea800c83e1f55a96687
OscarCsharp <Oscarndlovu01@gmail.com>
2024-08-11 22:30:43 +0200

    Update Program.cs

--- testdeploy/testdeploy/Program.cs
+++ testdeploy/testdeploy/Program.cs
@@ -15,9 +15,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.Urls.Add("http://*:80");
 app.Run();
